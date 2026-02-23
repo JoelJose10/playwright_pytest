3723e364cf838a886ff96e89169b630ded44f096
JoelJose10 <joeljos2000@gmail.com>
2026-02-23 12:03:58 +0530

    Removed delays

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def test_login_flow(page):
     page.goto(BASE_URL)
     page.fill("#user-name", VALID_USERNAME)
     page.fill("#password", VALID_PASSWORD)
-    page.wait_for_timeout(500)
+    # page.wait_for_timeout(500)
     page.click("#login-button")
 
 #Assignment 
@@ -20,21 +20,21 @@ def test_add_fleece_jacket_to_cart(page):
     #Log in
     test_login_flow(page)
     expect(page).to_have_url(f"{BASE_URL}/inventory.html")
-    page.wait_for_timeout(1500)
+    # page.wait_for_timeout(1500)
 
     #Adding Sauce Labs Fleece Jacket to cart
     page.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
-    page.wait_for_timeout(1500)
+    # page.wait_for_timeout(1500)
 
     #Verify cart badge shows 1 item
     cart_badge = page.locator(".shopping_cart_badge")
     expect(cart_badge).to_have_text("1")
-    page.wait_for_timeout(1500)
+    # page.wait_for_timeout(1500)
 
     # Go to cart
     page.click(".shopping_cart_link")
     expect(page).to_have_url(f"{BASE_URL}/cart.html")
-    page.wait_for_timeout(1500)
+    # page.wait_for_timeout(1500)
 
     #Verify the correct product is in the cart
     cart_item = page.locator(".cart_item")
@@ -42,4 +42,4 @@ def test_add_fleece_jacket_to_cart(page):
 
     product_name = page.locator(".inventory_item_name")
     expect(product_name).to_have_text("Sauce Labs Fleece Jacket")
-    page.wait_for_timeout(2000)
+    # page.wait_for_timeout(2000)
